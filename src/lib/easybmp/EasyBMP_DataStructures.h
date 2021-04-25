bf5a97987e99d1f5f171cf885ab002d3e38b2a93
kopwei <kopkop@gmail.com>
2021-04-25 20:35:59 +0200

    Reformat BMP code

--- src/lib/easybmp/EasyBMP_DataStructures.h
+++ src/lib/easybmp/EasyBMP_DataStructures.h
@@ -42,19 +42,24 @@ int IntPow( int base, int exponent );
 
 inline bool IsBigEndian()
 {
- short word = 0x0001;
- if((*(char *)& word) != 0x01 )
- { return true; }
- return false;
+    short word = 0x0001;
+    if ((*(char*) &word) != 0x01)
+    {
+        return true;
+    }
+
+    return false;
 }
 
-inline ebmpWORD FlipWORD( ebmpWORD in )
-{ return ( (in >> 8) | (in << 8) ); }
-
-inline ebmpDWORD FlipDWORD( ebmpDWORD in )
+inline ebmpWORD FlipWORD(ebmpWORD in)
 {
- return ( ((in&0xFF000000)>>24) | ((in&0x000000FF)<<24) | 
-          ((in&0x00FF0000)>>8 ) | ((in&0x0000FF00)<<8 )   );
+    return ((in >> 8) | (in << 8));
+}
+
+inline ebmpDWORD FlipDWORD(ebmpDWORD in)
+{
+    return (((in & 0xFF000000) >> 24) | ((in & 0x000000FF) << 24) |
+        ((in & 0x00FF0000) >> 8) | ((in & 0x0000FF00) << 8));
 }
 
 // it's easier to use a struct than a class
@@ -62,43 +67,48 @@ inline ebmpDWORD FlipDWORD( ebmpDWORD in )
 // at once (as we can count on them being continuous 
 // in memory
 
-typedef struct RGBApixel {
-	ebmpBYTE Blue;
-	ebmpBYTE Green;
-	ebmpBYTE Red;
-	ebmpBYTE Alpha;
-} RGBApixel; 
+typedef struct RGBApixel
+{
+    ebmpBYTE Blue;
+    ebmpBYTE Green;
+    ebmpBYTE Red;
+    ebmpBYTE Alpha;
+}
 
-class BMFH{
+RGBApixel;
+
+class BMFH
+{
 public:
- ebmpWORD  bfType;
- ebmpDWORD bfSize;
- ebmpWORD  bfReserved1;
- ebmpWORD  bfReserved2;
- ebmpDWORD bfOffBits; 
+    ebmpWORD bfType;
+    ebmpDWORD bfSize;
+    ebmpWORD bfReserved1;
+    ebmpWORD bfReserved2;
+    ebmpDWORD bfOffBits;
 
- BMFH();
- void display( void );
- void SwitchEndianess( void );
+    BMFH();
+    void display(void);
+    void SwitchEndianess(void);
 };
 
-class BMIH{
+class BMIH
+{
 public:
- ebmpDWORD biSize;
- ebmpDWORD biWidth;
- ebmpDWORD biHeight;
- ebmpWORD  biPlanes;
- ebmpWORD  biBitCount;
- ebmpDWORD biCompression;
- ebmpDWORD biSizeImage;
- ebmpDWORD biXPelsPerMeter;
- ebmpDWORD biYPelsPerMeter;
- ebmpDWORD biClrUsed;
- ebmpDWORD biClrImportant;
+    ebmpDWORD biSize;
+    ebmpDWORD biWidth;
+    ebmpDWORD biHeight;
+    ebmpWORD biPlanes;
+    ebmpWORD biBitCount;
+    ebmpDWORD biCompression;
+    ebmpDWORD biSizeImage;
+    ebmpDWORD biXPelsPerMeter;
+    ebmpDWORD biYPelsPerMeter;
+    ebmpDWORD biClrUsed;
+    ebmpDWORD biClrImportant;
 
- BMIH();
- void display( void );
- void SwitchEndianess( void );
+    BMIH();
+    void display(void);
+    void SwitchEndianess(void);
 };
 
 #endif
